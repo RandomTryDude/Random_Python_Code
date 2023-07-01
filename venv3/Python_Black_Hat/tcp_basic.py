a243cd805fddc60bf65f64d9a6fce8389aab7765
RandomTryDude <128506433+RandomTryDude@users.noreply.github.com>
2023-07-01 23:23:43 +0000

    good

--- venv3/Python_Black_Hat/tcp_basic.py
+++ venv3/Python_Black_Hat/tcp_basic.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "127.0.0.1"
-target_port = 9998
+target_host = "127.0.0.1" # adresse d'envoie
+target_port = 9998 # port d'écoute
 
 #create a socket object
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -13,10 +13,12 @@ client.connect((target_host,target_port))
 
 # send some data
 client.send(b"GET / HTTP/1.1\r\nHost:google.com\r\n\r\n")
+# message envoyé
 
 # receive some data
 
 response = client.recv(4096)
+# recois réponse de notre serveur
 
 print(response.decode())
 client.close()
