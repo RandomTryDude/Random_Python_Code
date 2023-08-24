6ea540416655470fd0069ee671b5d8b66d415505
RandomTryDude <128506433+RandomTryDude@users.noreply.github.com>
2023-08-24 12:10:16 +0000

    SQL Learning + reorg

---
+++ SQL_Py/SQL_Python_Create_Table.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+# Créer une connexion à la base de données (ou la créer si elle n'existe pas)
+conn = sqlite3.connect('Sql_Test.db')
+cursor = conn.cursor()
+
+# Créer la table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS MaTable (
+        identifiant INT PRIMARY KEY,
+        prenom TEXT,
+        nom TEXT,
+        ville TEXT
+    )
+''')
+
+# Insérer des données dans la table
+donnees = [
+    (1, 'Pierre', 'Dupond', 'Paris'),
+    (2, 'Sabrina', 'Durand', 'Nantes'),
+    (3, 'Julien', 'Martin', 'Lyon'),
+    (4, 'David', 'Bernard', 'Marseille'),
+    (5, 'Marie', 'Leroy', 'Grenoble')
+]
+
+cursor.executemany('INSERT INTO MaTable (identifiant, prenom, nom, ville) VALUES (?, ?, ?, ?)', donnees)
+
+# Valider les changements et fermer la connexion
+conn.commit()
+conn.close()
